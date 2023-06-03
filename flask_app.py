eb6ecdb418ff76661d6c8a14c29fbeed576b76e6
blankdvth <contact@blankdvth.com>
2023-06-03 00:21:06 -0400

    Remove oembed, add video

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 from json import loads
 from json.decoder import JSONDecodeError
 
-from flask import Flask, render_template, jsonify, abort
+from flask import Flask, render_template, abort
 from lzstring import LZString
 
 app = Flask(__name__)
@@ -21,30 +21,7 @@ def index():
 
 @app.route('/<data>')
 def show(data: str):
-    # TODO: Handle video
-    print(load_data(data))
     return render_template("show.html", data=load_data(data), raw_data=data)
-
-
-@app.route('/oembed/<data>')
-def oembed(data: str):
-    data = load_data(data.rstrip(".json"))
-
-    oembed_data = {}
-    if "title" in data:
-        oembed_data["title"] = data["title"]
-    if "author" in data:
-        oembed_data["author_name"] = data["author"]
-    if "author_url" in data:
-        oembed_data["author_url"] = data["author_url"]
-    if "provider" in data:
-        oembed_data["provider_name"] = data["provider"]
-    if "provider_url" in data:
-        oembed_data["provider_url"] = data["provider_url"]
-    if "large_image" in data and data["large_image"] and "description" not in data:
-        oembed_data["type"] = "photo"
-
-    return jsonify(oembed_data)
 
 
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -1,3 +1,16 @@
+window.onbeforeunload = function (e) { // We reset the page on load, let's warn them.
+    if (checkForChanges()) return "Are you sure you want to leave? Your changes WILL be lost.";
+}
+
+const colourPicker = document.getElementById("colour");
+const hexColourPicker = document.getElementById("colour-hex");
+const mediaSelect = document.getElementById("media-type");
+const mediaInput = document.getElementById("media");
+const mediaHelp = document.getElementById("media-help");
+const mediaImageIcon = document.getElementById("image-outline-icon");
+const mediaVideoIcon = document.getElementById("video-outline-icon");
+const largeImageField = document.getElementById("large-image-field");
+
 /**
  * Called upon user input in input element, used to handle validation.
  * If an element with an ID of `elementId-help` exists, it will be shown/hidden based on the validity of the input.
@@ -31,24 +44,65 @@ function handleInput(element, regex= null) {
  * @param changed {HTMLInputElement} The input element that was changed.
  */
 function handleColourInput(changed) {
-    const picker = document.getElementById("colour");
-    const hex = document.getElementById("colour-hex");
-    picker.value = changed.value;
-    picker.dataset.valid = changed.dataset.valid;
-    hex.value = changed.value;
-    hex.dataset.valid = changed.dataset.valid;
+    colourPicker.value = changed.value;
+    colourPicker.dataset.valid = changed.dataset.valid;
+    hexColourPicker.value = changed.value;
+    hexColourPicker.dataset.valid = changed.dataset.valid;
+}
+
+/**
+ * Called to handle media inputs as appropriate based on the type of media.
+ * @param element {HTMLInputElement | HTMLSelectElement}
+ */
+function handleMediaInput(element) {
+    const mediaType = mediaSelect.value;
+    if (element.nodeName === "SELECT") {
+        if (mediaType === "Image") {
+            largeImageField.classList.remove("is-hidden");
+            mediaImageIcon.classList.remove("is-hidden");
+            mediaVideoIcon.classList.add("is-hidden");
+            mediaInput.placeholder = mediaInput.dataset.placeholderImage;
+            mediaHelp.innerText = mediaHelp.dataset.image;
+        } else if (mediaType === "Video") {
+            largeImageField.classList.add("is-hidden");
+            mediaImageIcon.classList.add("is-hidden");
+            mediaVideoIcon.classList.remove("is-hidden");
+            mediaInput.placeholder = mediaInput.dataset.placeholderVideo;
+            mediaHelp.innerText = mediaHelp.dataset.video;
+        }
+    }
+    if (mediaType === "Image") {
+        handleInput(mediaInput, /^https?:\/\/.*\/.*\.(png|gif|webp|jpeg|jpg)\??.*$/i)
+    } else if (mediaType === "Video") {
+        handleInput(mediaInput, /^https?:\/\/.*\/.*\.(mp4|webm|mov)\??.*$/i)
+    }
+}
+
+/**
+ * Checks if values have changed from their default values
+ */
+function checkForChanges() {
+    const elements = [...document.querySelectorAll("input, textarea, select")];
+    return elements.some(element => {
+        if (element.type === "checkbox") {
+            return element.checked !== (element.dataset.default === "true");
+        }
+        return element.value !== (element.dataset.default ?? "");
+    });
 }
 
 /**
  * Reset all form elements to their default values (data-default). Will be emptied if no data-default is set.
  */
 function resetToDefault() {
-    document.querySelectorAll("input, textarea").forEach(element => {
+    document.querySelectorAll("input, textarea, select").forEach(element => {
         if (element.type === "checkbox") {
             element.checked = element.dataset.default === "true";
             return;
         }
         element.value = element.dataset.default ?? "";
+        // noinspection JSCheckFunctionSignatures - We have a custom oninput that takes an element as an argument.
+        element.oninput?.(element);
     })
     document.getElementById("url-box").classList.add("is-hidden");
 }
@@ -57,7 +111,7 @@ function resetToDefault() {
  * Generates the output URL and displays it to the user.
  */
 function generate() {
-    const elements = [...document.querySelectorAll("input, textarea")];
+    const elements = [...document.querySelectorAll("input, textarea, select")];
     if (!elements.every(element => handleInput(element))) return;
     // Generate JSON consisting of pairs of ID:value
     const data = {};
@@ -129,3 +183,5 @@ function unfade(element) {
         op += op * 0.1;
     }, 0.05);
 }
+
+resetToDefault(); // Clear all inputs on page load
